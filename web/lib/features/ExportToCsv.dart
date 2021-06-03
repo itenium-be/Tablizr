17f90fa499424601cd38bf5e346a4fa7d1ca3b2d
Jonathan Rasquin <jonathan.rasquin@rodekruis.be>
2021-06-03 22:31:23 +0200

    first commit

--- web/lib/features/ExportToCsv.dart
+++ web/lib/features/ExportToCsv.dart
@@ -1,4 +1,7 @@
 import 'dart:html';
+//import 'dart:io';
+import 'package:csv/csv.dart';
+
 import 'Feature.dart';
 import '../TableModel.dart';
 
@@ -8,5 +11,20 @@ class ExportToCsv extends Feature {
   void enhance() {
     print(
         'ExportToCsv: Font-Awesome is loaded: <i class="fas fa-file-csv"></i>');
+    var a = AnchorElement();
+    a.text = 'CSV';
+    a.onClick.listen((e) async {
+      print('Downloading CSV');
+      var test = table.table.tHead?.rows.first.cells as List<TableCellElement>;
+      for (TableCellElement row in test) {}
+
+      //String csv = const ListToCsvConverter().convert(yourListOfLists);
+      //final filename = 'file.txt';
+      var file = await File(filename).writeAsString('some content');
+      //final pathOfTheFileToWrite = directory.path + "/myCsvFile.csv"
+      //File file = await File(pathOfTheFileToWrite);
+      //file.writeAsString(csv);
+    });
+    table.footer.children.add(a);
   }
 }
