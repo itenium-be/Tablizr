0b01fd615b1b719a4619fba3a57c67f443a71e20
Wouter Van Schandevijl <woutervs@hotmail.com>
2023-02-01 14:44:57 +0100

    add pagination

--- web/lib/features/Pagination.dart
+++ web/lib/features/Pagination.dart
@@ -7,5 +7,15 @@ class Pagination extends Feature {
 
   void enhance() {
     print('Pagination: add footer pagination');
+
+    var pags = SelectElement();
+    var opts = [1, 2, 3, 4, 5].map((opt) {
+      var optEl = OptionElement();
+      optEl.value = opt.toString();
+      optEl.text = opt.toString();
+      return optEl;
+    });
+    pags.children.addAll(opts);
+    table.footer.children.add(pags);
   }
 }
